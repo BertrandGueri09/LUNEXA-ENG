1cd079507d703dd08f4eba568a6dcbd8b79714c6
NGAGABA Bertrand Gueri <bertrandgueri@gmail.com>
2026-05-31 16:57:57 +0000

    Update page title and icon for Streamlit app

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="English Learning Coach",
-    page_icon="🇬🇧",
+    page_title="LUNEXA ENG",
+    page_icon="LE",
     layout="wide"
 )
 
